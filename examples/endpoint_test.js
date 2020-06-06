9429cd54aef751a0d18518eecfd873f9d83539aa
Cris Mihalache <me@f3rno.com>
2020-06-06 21:52:48 +0700

    (refactor) improve docs, tweak internal structure

--- examples/endpoint_test.js
+++ examples/endpoint_test.js
@@ -4,8 +4,13 @@ process.env.DEBUG = 'bfx:*'
 
 const assert = require('assert')
 const debug = require('debug')('bfx:api:mock-srv:examples:endpoint-test')
-const { MockRESTv2Server } = require('../')
 const { RESTv2 } = require('bfx-api-node-rest')
+const { MockRESTv2Server } = require('../')
+
+const FUNDING_OFFER = [
+  41215275, 'fUSD', 1524784806000, 1524784806000, 1000, 1000, 'FRRDELTAVAR',
+  null, null, 0, 'ACTIVE', null, null, null, 0, 30, 0, 0, null, 0, 0.00207328
+]
 
 debug('spawning mock server...')
 
@@ -16,20 +21,17 @@ const rest = new RESTv2({
   url: 'http://localhost:9999'
 })
 
-const fundingOffer = [
-  41215275, 'fUSD', 1524784806000, 1524784806000, 1000, 1000, 'FRRDELTAVAR',
-  null, null, 0, 'ACTIVE', null, null, null, 0, 30, 0, 0, null, 0, 0.00207328
-]
-
-srv.setResponse('f_offers.fUSD', [fundingOffer])
+srv.setResponse('f_offers.fUSD', [FUNDING_OFFER])
 
 debug('requesting preset response...')
 
 rest.fundingOffers('fUSD').then(([incomingFundingOffer]) => {
-  assert.deepStrictEqual(incomingFundingOffer, fundingOffer)
+  assert.deepStrictEqual(incomingFundingOffer, FUNDING_OFFER)
 
   debug('correct response received')
   srv.close()
+
+  return null
 }).catch((e) => {
   debug(`error: ${e.message}`)
 })
